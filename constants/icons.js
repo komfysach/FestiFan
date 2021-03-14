baed643fbf09ae2d348e322b110853e6760ce2b6
komfysach <sach@getkomfy.net>
2021-03-14 21:09:48 +0200

    set-up react native app, added icons and fonts

---
+++ constants/icons.js
@@ -0,0 +1,43 @@
+const back = require('../assets/icons/back.png');
+const drinks = require('../assets/icons/drinks.png');
+const food = require('../assets/icons/food.png');
+const artists = require('../assets/icons/artists.png');
+const map = require('../assets/icons/map.png');
+const alert = require('../assets/icons/alert.png');
+const tickets = require('../assets/icons/tickets.png');
+const down = require('../assets/icons/down.png');
+const eye = require('../assets/icons/eye.png');
+const eyeclose = require('../assets/icons/eye-close.png');
+const close = require('../assets/icons/close.png');
+const bin = require('../assets/icons/bin.png');
+const settings = require('../assets/icons/settings.png');
+const scan = require('../assets/icons/settings.png');
+const card = require('../assets/icons/card.png');
+const wallet = require('../assets/icons/wallet.png');
+const password = require('../assets/icons/password.png');
+const profile = require('../assets/icons/profile.png');
+const reload = require('../assets/icons/reload.png');
+const time = require('../assets/icons/time.png');
+
+export default {
+    back,
+    drinks,
+    food,
+    tickets,
+    reload,
+    wallet,
+    alert,
+    bin,
+    close,
+    down,
+    scan,
+    settings,
+    eye,
+    eyeclose,
+    card,
+    profile,
+    password,
+    time,
+    map,
+    artists
+};
